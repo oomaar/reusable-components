1ff67923c351a90dc3dcd2212e4bf222ef6a45a5
oomaar <omaar_5@hotmail.com>
2025-09-22 12:27:38 +0300

    feat(App.tsx): define app routes and render App's main Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/layout/Layout";
 
 function App() {
-  return <Routes>Hello World</Routes>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<div>Hello World</div>} />
+        <Route path="/table" element={<div>Table</div>} />
+        <Route path="/auto-select" element={<div>auto-select</div>} />
+        <Route path="/media-upload" element={<div>media-upload</div>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
